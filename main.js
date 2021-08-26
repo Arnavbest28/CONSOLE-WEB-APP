49c9b1ec5805ac64db2fcd4c5bc92ee2dee9aaf5
Arnavbest28 <82748086+Arnavbest28@users.noreply.github.com>
2021-08-26 14:55:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var first_name ="Arnav"; 
+console.log(first_name);
+var last_name ="Gupta";
+console.log(last_name);
+var full_name= first_name.concat(last_name);
+console.log(full_name);
+
+var a="6";
+var b="5";
+var add = a + b;
+console.log(add);
